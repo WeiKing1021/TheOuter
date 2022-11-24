e483ce119a6cf853757504b8ddb93b90c61bfe59
WeiKing1021 <WeiKing1021@gmail.com>
2022-11-24 16:01:18 +0800

    test FixedUpdate() method to rotate toggler and add VRTK dependency

--- Assets/Scripts/Toggler.cs
+++ Assets/Scripts/Toggler.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class Toggler : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-
-        this.transform.rotation.Set(-90F, 0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = this.transform.rotation.x;
-        float y = this.transform.rotation.y;
-        float z = this.transform.rotation.z;
-        float w = this.transform.rotation.w;
+    }
 
-        x += 10;
+    private void FixedUpdate()
+    {
 
-        this.transform.rotation.Set(x, y, z, w);
-
-        System.Console.WriteLine("Update x to " + x);
+        this.transform.Rotate(new Vector3(1, 0, 0));
     }
 }
